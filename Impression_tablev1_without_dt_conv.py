0e95bc141e83f54c9231975e23dfe4dd53b6ffdf
TechProf LTD <4683712+techprof@users.noreply.github.com>
2023-06-12 18:30:10 +0100

    Update Impression_tablev1_without_dt_conv.py

--- Impression_tablev1_without_dt_conv.py
+++ Impression_tablev1_without_dt_conv.py
@@ -3,37 +3,49 @@ from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from datetime import datetime, timedelta
 
-# Initialize GlueContext and SparkContext
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+# Initialize GlueContext and SparkSession
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
 
-# Create a dynamic frame from the existing table
-dynamic_frame = glueContext.create_dynamic_frame.from_catalog(
-    database="your_database_name",
-    table_name="your_table_name"
-)
+# Retrieve arguments passed from the Glue job
+args = getResolvedOptions(sys.argv, ['DATABASE', 'SOURCE_TABLE', 'TARGET_TABLE'])
 
-# Convert the dynamic frame to a Spark DataFrame
-data_frame = dynamic_frame.toDF()
+# Get the source table from the Glue Data Catalog
+database = args['DATABASE']
+source_table = args['SOURCE_TABLE']
+target_table = args['TARGET_TABLE']
 
-# Filter out null data
-filtered_data_frame = data_frame.filter(data_frame.your_column_name.isNotNull())
+# Calculate the date 90 days ago
+end_date = datetime.now().date()
+start_date = end_date - timedelta(days=90)
 
-# Create a new dynamic frame from the filtered DataFrame
-new_dynamic_frame = DynamicFrame.fromDF(filtered_data_frame, glueContext, "new_dynamic_frame")
+# Read the source table data from Glue Data Catalog
+source_data_frame = glueContext.create_dynamic_frame.from_catalog(database=database, table_name=source_table)
 
-# Write the new dynamic frame to a new Athena table
-glueContext.write_dynamic_frame.from_catalog(
-    frame=new_dynamic_frame,
-    database="your_database_name",
-    table_name="new_table_name",
-    transformation_ctx="write_dynamic_frame"
-)
+# Convert the source dynamic frame to a Spark DataFrame
+source_df = source_data_frame.toDF()
 
-job.commit()
+# Apply the filter to get data within the 90-day range and remove null data
+filtered_df = source_df.filter((col("year") > start_date.year) |
+                               (col("year") == start_date.year) & (col("month") > start_date.month) |
+                               (col("year") == start_date.year) & (col("month") == start_date.month) &
+                               (col("day") >= start_date.day))
+
+filtered_df = filtered_df.dropna()
+
+# Create a dynamic frame from the filtered Spark DataFrame
+target_dynamic_frame = glueContext.create_dynamic_frame.from_catalog(database=database, table_name=source_table)
+
+# Write the dynamic frame to a new table in the Glue Data Catalog
+glueContext.write_dynamic_frame.from_catalog(dynamic_frame=target_dynamic_frame,
+                                             database=database,
+                                             table_name=target_table,
+                                             transformation_ctx="target")
+
+# Print the schema of the new table
+print(f"New table schema: {target_dynamic_frame.schema()}")
